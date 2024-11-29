34dde38b2a48773acfd9dda6651a79743331cce8
Philip Rosenqvist <philip.rosenqvist@outlook.com>
2024-11-29 16:14:08 +0100

    Removed boilerplate

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
-import './assets/main.css'
+import './assets/style.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router/index.js'
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(router)
+app.use(router);
 
-app.mount('#app')
+app.mount('#app');
