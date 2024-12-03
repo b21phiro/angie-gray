6639a2f87bc426fb06f3f51359d7afe36d6d5fa5
Philip Rosenqvist <philip.rosenqvist@outlook.com>
2024-12-03 20:22:33 +0100

    did something i guess

--- src/components/inputs/Email.vue
+++ src/components/inputs/Email.vue
@@ -1,11 +1,36 @@
 <script setup>
   import './input.css';
+  import { ref } from "vue";
+
+  const emailRegExp = new RegExp(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/);
+  const error = ref(false);
+
+  function onBlur(ev) {
+    const { target: input } = ev;
+    validate(input);
+  }
+
+  function validate(input) {
+    error.value = !!emailRegExp.test(input.value);
+    return error.value;
+  }
+
+  defineExpose({
+    error,
+    validate
+  });
+
 </script>
 
 <template>
   <div class="input">
     <label class="input--label" for="email">Your email</label>
-    <input class="input--value" id="email" type="text" name="email" />
-    <label class="input--info" for="email">Enter a valid email address,</label>
+    <input @blur="onBlur"
+           class="input--value"
+           id="email"
+           type="text"
+           name="email"
+    />
+    <label v-if="!error" class="input--info" for="email">Enter a valid email address.</label>
   </div>
 </template>
